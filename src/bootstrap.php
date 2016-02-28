4f99b2e71cb6dfa28dd742d4b5f21347ebfbb69c
Konstantin Chukhlomin <chuhlomin@gmail.com>
2016-02-28 01:09:20 -0500

    Fixes and improvements
    Delay now is for typing messages, not for reading
    Indication typing message
    Showing keyboard buttons in a row
    Hiding keyboard for messages which not requiring response
    Repeat of options if received message not recognized (and send only options)
    A lot of debug logging

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('America/New_York');
+
 $parser = new \Symfony\Component\Yaml\Parser();
 
 $configFileContent = file_get_contents(BASEDIR . '/config/default.yaml');
@@ -7,7 +9,9 @@ $configFileContent = file_get_contents(BASEDIR . '/config/default.yaml');
 $config = $parser->parse($configFileContent);
 
 $monolog = new \Monolog\Logger('telegram_bot');
-$monolog->pushHandler(new \Monolog\Handler\StreamHandler('/var/log/telegram-bot-experiment/hook.log'));
+$monolog->pushHandler(
+    new \Monolog\Handler\StreamHandler('/var/log/telegram-bot-experiment/hook.log', \Monolog\Logger::INFO)
+);
 
 $memcached = new Memcached();
 $memcached->addServer('localhost', 11211);
--- www/hook.php
+++ www/hook.php
@@ -22,7 +22,7 @@ try {
 
     $monolog->addInfo(
         sprintf(
-            'user_id:%s state:%s: %s',
+            'user_id:%s state:%s message:%s',
             $userID,
             $stateID,
             $message
@@ -32,33 +32,61 @@ try {
     $state = new \src\models\State($script, $stateID);
 
     $messages = [];
-    $stateChanged = false;
+    $shouldGetBotMessages = true;
 
-    try {
-        $stateID = $state->getFollowupByMessage($message);
-        $stateChanged = true;
-    } catch (\src\exceptions\UserMessageNotFoundInAvailableOptions $e) {
-        $messages[] = 'Sorry, did not catch what just you said. Could you pick from provided options?';
-    }
-
-    if ($stateChanged) {
-        $state = new \src\models\State($script, $stateID);
-
-        $memcached->set('state:' . $userID, $stateID);
-
-        $messages[] = $state->getBotMessage();
-
-        while (($stateID = $state->getFollowup()) !== null) {
+    if ($script['start'] !== $stateID) { // not first state
+        $monolog->addDebug(sprintf('This is not start state ID, so, trying to find next state by message'));
+        try {
+            $stateID = $state->getFollowupByMessage($message);
+            $monolog->addDebug(sprintf('Next state is: %s', $stateID));
             $state = new \src\models\State($script, $stateID);
-            $messages[] = $state->getBotMessage();
+        } catch (\src\exceptions\UserMessageNotFoundInAvailableOptions $e) {
+            $monolog->addDebug(sprintf('Cold not get next state from state \'%s\' for message: %s ', $stateID, $message));
+            $messages[] = 'Sorry, did not catch what just you said. Could you pick from provided options?';
+            $shouldGetBotMessages = false;
         }
     }
 
+    if ($shouldGetBotMessages) {
+        $botMessage = $state->getBotMessage();
+        $monolog->addDebug(sprintf('Adding new bot message to pool: %s', $botMessage));
+        $messages[] = $botMessage;
+
+        while (($nextStateID = $state->getFollowup()) !== null) {
+            $monolog->addDebug(sprintf('Next state ID: %s', $nextStateID));
+            $state = new \src\models\State($script, $nextStateID);
+            $stateID = $nextStateID;
+            $botMessage = $state->getBotMessage();
+            $monolog->addDebug(sprintf('Adding new bot message to pool: %s', $botMessage));
+            $messages[] = $botMessage;
+        }
+    }
+
+    $monolog->addDebug(sprintf('Updating user state: %s', $stateID));
+    $memcached->set('state:' . $userID, $stateID);
+
+    $monolog->addDebug(sprintf('Trying to get options for state: %s', $stateID));
     $options = $state->getResponseOptions();
+    $monolog->addDebug(sprintf('User options: %s', json_encode($options)));
 
     $lastMessage = end($messages);
+    $monolog->addDebug(sprintf('Last message should be: %s', $lastMessage));
+
     foreach ($messages as $message) {
-        $isLastMessage = $message === $lastMessage;
+        $monolog->addDebug(sprintf('Current message: %s', $message));
+        $isLastMessage = ($message === $lastMessage);
+        $monolog->addDebug(sprintf('Is last: %s', var_export($isLastMessage, true)));
+        $monolog->addDebug(sprintf('Options are: %s', json_encode($options)));
+
+        $response = $telegram->sendChatAction(
+            [
+                'chat_id' => $chatID,
+                'action' => 'typing'
+            ]
+        );
+
+        // avg person write 250 words in a minute -> 250 words in a 60 sec -> 4.16666667 in sec
+        sleep(str_word_count($message) / 4.2);
 
         if ($isLastMessage) {
             $response = $telegram->sendMessage([
@@ -75,14 +103,24 @@ try {
         } else {
             $response = $telegram->sendMessage([
                 'chat_id' => $chatID,
-                'text' => $message
+                'text' => $message,
+                'reply_markup' => $telegram->replyKeyboardHide(
+                    [
+                        'hide_keyboard' => true
+                    ]
+                )
             ]);
-
-            // 200 words in a minute -> 200 words in a 60 sec -> 3.33333333 in sec
-            sleep(str_word_count($message) / 3.33);
         }
     }
 } catch (\Exception $e) {
     $message = 'Error...';
-    $monolog->addError($e->getMessage());
+    $monolog->addError(
+        sprintf(
+            '%s [%s:%s]: %s',
+            get_class($e),
+            $e->getFile(),
+            $e->getLine(),
+            $e->getMessage()
+        )
+    );
 }
